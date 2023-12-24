8ac71b13a5327034320319283b9d2fa7eba300b2
Sergei Voinarenko <voynarenko@gmail.com>
2023-12-24 16:19:33 +0300

    Enemy animations, follow and aggro added.

---
+++ Assets/Scripts/Enemy/Aggro.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemy
+{
+    public class Aggro : MonoBehaviour
+    {
+        public TriggerObserver TriggerObserver;
+        public EnemyMoveToPlayer Follow;
+
+        public float Cooldown;
+        private Coroutine _aggroCoroutine;
+        private bool _hasAggroTarget;
+
+        private void Start()
+        {
+            TriggerObserver.TriggerEnter += TriggerEnter;
+            TriggerObserver.TriggerExit += TriggerExit;
+
+            SwitchFollow(false);
+        }
+
+        private void TriggerEnter(Collider other)
+        {
+            if (_hasAggroTarget) return;
+            _hasAggroTarget = true;
+            StopAggroCoroutine();
+            SwitchFollow(true);
+        }
+
+        private void TriggerExit(Collider other)
+        {
+            if (!_hasAggroTarget) return;
+            _hasAggroTarget = false; 
+            _aggroCoroutine = StartCoroutine(SwitchFollowOffAfterCooldown());
+        }
+
+        private void StopAggroCoroutine()
+        {
+            if (_aggroCoroutine == null) return;
+            StopCoroutine(_aggroCoroutine);
+            _aggroCoroutine = null;
+        }
+
+        private IEnumerator SwitchFollowOffAfterCooldown()
+        {
+            yield return new WaitForSeconds(Cooldown);
+            SwitchFollow(false);
+        }
+
+        private void SwitchFollow(bool value) => 
+            Follow.enabled = value;
+    }
+}
---
+++ Assets/Scripts/Enemy/AnimateAlongAgent.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.Enemy
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    [RequireComponent(typeof(EnemyAnimator))]
+    public class AnimateAlongAgent : MonoBehaviour
+    {
+        private const float MinimalVelocity = 0.1f;
+        
+        public NavMeshAgent Agent;
+        public EnemyAnimator Animator;
+
+        private void Update()
+        {
+            if(ShouldMove())
+                Animator.Move();
+            else
+                Animator.StopMoving();
+        }
+
+        private bool ShouldMove() => 
+            Agent.velocity.magnitude > MinimalVelocity && Agent.remainingDistance > Agent.radius;
+    }
+}
