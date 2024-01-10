ad48b2907f7c908359c5201dd4d0610fd0d29899
Sergei Voinarenko <voynarenko@gmail.com>
2024-01-10 15:59:42 +0300

    Footsteps audio bug fixed.

--- Assets/Scripts/Player/PlayerDirectionFinder.cs
+++ Assets/Scripts/Player/PlayerDirectionFinder.cs
@@ -4,43 +4,21 @@ namespace Assets.Scripts.Player
 {
     public class PlayerDirectionFinder : MonoBehaviour
     {
+        private Animator Animator => GetComponent<Animator>();
         private PlayerAnimation PlayerAnimation => GetComponent<PlayerAnimation>();
         private float _horizontal;
         private float _vertical;
 
         public int GetDirection()
         {
-            var result = 0;
-            _horizontal = PlayerAnimation.AnimIdHorizontal;
-            _vertical = PlayerAnimation.AnimIdVertical;
+            var result = 99;
+            _horizontal = Animator.GetFloat(PlayerAnimation.AnimIdHorizontal);
+            _vertical = Animator.GetFloat(PlayerAnimation.AnimIdVertical);
 
-            switch (_vertical)
-            {
-                case > 0:
-                {
-                    if (_vertical > Mathf.Abs(_horizontal)) result = 0;
-                    break;
-                }
-                case < 0:
-                {
-                    if (Mathf.Abs(_vertical) > Mathf.Abs(_horizontal)) result = 1;
-                    break;
-                }
-            }
-
-            switch (_horizontal)
-            {
-                case > 0:
-                {
-                    if (_horizontal > Mathf.Abs(_vertical)) result = 2;
-                    break;
-                }
-                case < 0:
-                {
-                    if (Mathf.Abs(_horizontal) > Mathf.Abs(_vertical)) result = 3;
-                    break;
-                }
-            }
+            if (_vertical > 0 && _vertical > Mathf.Abs(_horizontal)) result = 0;
+            else if (_vertical < 0 && Mathf.Abs(_vertical) > Mathf.Abs(_horizontal)) result = 1;
+            else if (_horizontal > 0 && _horizontal > Mathf.Abs(_vertical)) result = 2;
+            else if (_horizontal < 0 && Mathf.Abs(_horizontal) > Mathf.Abs(_vertical)) result = 3;
 
             return result;
         }
